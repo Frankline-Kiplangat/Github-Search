49e4fe7390621a66a7641ac7ad6813593ccde27f
Frank <kipfrankline@gmail.com>
2020-06-22 13:27:08 +0300

    Add about and Not-found Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { GithubComponent } from './github/github.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'github', component: GithubComponent},
+  { path: 'about', component: AboutComponent},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { GithubService } from './services/github.service';
 import { GithubComponent } from './github/github.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GithubFormComponent } from './github-form/github-form.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component'
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
     NavbarComponent,
     GithubFormComponent,
+    NotFoundComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
